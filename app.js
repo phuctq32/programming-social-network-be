be7bcb3c6b2d5c26b3dd296bca21327887bbda1e
phuctq32 <phuctq32@gmail.com>
2022-12-05 22:22:44 +0700

    setup middleware

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
-
-const app = express();
+const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 
 const MONGODB_SERVER_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.pfnzbiy.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use((error, req, res, next) => {
+    const { statusCode, message, data, validationErrors } = err;
+    res.status(statusCode || 500).json({ message, data, validationErrors });
+});
+
 mongoose
     .connect(MONGODB_SERVER_URL)
     .then(result => {
