8f13bee71a10193b52b3c960da4e1a5085a1d3be
phuctq32 <phuctq32@gmail.com>
2022-12-05 20:14:19 +0700

    completed setup mongoose

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Connected!');
-})
+const mongoose = require('mongoose');
+
+const MONGODB_SERVER_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.pfnzbiy.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
+mongoose
+    .connect(MONGODB_SERVER_URL)
+    .then(result => {
+        app.listen(process.env.PORT || 8080);
+        console.log('connected');
+    })
+    .catch(err => {
+        console.log(err);
+    });
