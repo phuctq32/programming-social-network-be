79e529915dd23d5bfd1c5256f68d7385d9113cb9
phuctq32 <phuctq32@gmail.com>
2022-12-07 02:44:13 +0700

    change configuratuon

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
+
+dotenv.config();
 
 const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.pfnzbiy.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
 
