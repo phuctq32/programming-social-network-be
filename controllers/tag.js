e4ab3940f79bac26ad4b41237e7fcd1adf828703
phuctq32 <phuctq32@gmail.com>
2022-12-09 23:36:48 +0700

    add create tag api

--- controllers/tag.js
+++ controllers/tag.js
@@ -1,3 +1,4 @@
+import { validationResult } from 'express-validator';
 import Category from '../models/category.js';
 import Tag from '../models/tag.js';
 
@@ -11,11 +12,55 @@ export const getTagsByCategoryId = async (req, res, next) => {
             return next(error);
         }
 
-        const tags = await Tag.find({ category: categoryId});
+        const tags = await Tag.find({ category: categoryId });
 
         res.status(200).json({
             tags,
             totalTags: tags.length
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const createTag = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error(errors.array()[0].msg);
+        error.statusCode = 422;
+        error.validationErrors = errors.array();
+        return next(error);
+    }
+
+    const categoryId = req.body.categoryId;
+    const tagName = req.body.tag;
+    try {
+        const category = await Category.findById(categoryId);
+        if (!category) {
+            const error = new Error('Category not found.');
+            error.statusCode = 404;
+            return next(error);
+        }
+
+        // check if the tag is existing
+        const existingTag = await Tag.findOne({
+            category: categoryId,
+            name: tagName
+        });
+        if (existingTag) {
+            const error = new Error('Tag is existing.');
+            error.statusCode = 409;
+            return next(error);
+        }
+
+        const newTag = new Tag({
+            name: tagName,
+            category: categoryId
+        });
+        await newTag.save();
+
+        res.status(201).json({
+            message: 'Created tag successfully.'
         });
     } catch (err) {
         next(err);
--- routes/tag.js
+++ routes/tag.js
@@ -3,11 +3,19 @@ import { body } from 'express-validator';
 import * as tagController from '../controllers/tag.js';
 import isAdmin from '../middlewares/is-admin.js';
 import isAuth from '../middlewares/is-auth.js';
+import Tag from '../models/tag.js';
 
 const router = Router();
 
-router.get('/tags/:categoryId', tagController.getTagsByCategoryId);
+router.get('/:categoryId/tags', tagController.getTagsByCategoryId);
 
-router.post('/tag/:categoryId', isAuth);
+router.post(
+    '/tag',
+    isAuth,
+    [
+        body('name')
+            .notEmpty().withMessage('The name of tag is required.')
+    ],  
+    tagController.createTag);
 
 export default router;
